dd3e4a5797f31bad7d33922995a2f907d0de0f34
bgoodson <bgoodson@kilo.local>
2021-11-06 13:32:52 -0400

    patch for new arch

--- src/index.js
+++ src/index.js
@@ -38,7 +38,13 @@ const app = express()
 const origin = CORS_ORIGIN
 
 const postgresStoreConfig = () => {
-  const conString = NODE_ENV === 'dev' ? `${POSTGRES_CONN_STR}?sslmode=no-verify` : `${POSTGRES_CONN_STR}`
+  const conString = NODE_ENV === 'dev' ? `${POSTGRES_CONN_STR}?sslmode=no-verify` : `${POSTGRES_CONN_STR}?sslmode=require`
+  const ssl = NODE_ENV === 'dev' ? {
+    require: true,
+    rejectUnauthorized: false
+  } : {
+    require: true
+  }
   if (NODE_ENV === 'dev') {
     return {
       conString
